8f67ac9b21fb51101a143c03dde705f2288f68ed
Rachelpoundcakes <115759222+Rachelpoundcakes@users.noreply.github.com>
2022-10-16 00:57:22 +0900

    Delete Samples directory

--- Samples/sample-03.py
+++
@@ -1,5 +0,0 @@
-from gtts import gTTS
-text =  "안녕하세요 마이크로소프트 에이아이 스쿨에 오신 것을 환영합니다."
-
-tts = gTTS(text=text, lang='ko')
-tts.save("hello.mp3")
--- Samples/test.ww.py
+++
@@ -1,6 +0,0 @@
-import GTTS
-text =  "안녕하세요 마이크로소프트 에이아이 스쿨에 오신 것을 환영합니다."
-
-tts = GTTS(text=text, lang='ko')
-tts.save(r"hello.mp3")
-
