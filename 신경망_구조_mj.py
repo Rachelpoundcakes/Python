86e2db40fa7901d696404217a697dfada10e5e0c
Rachelpoundcakes <rachelkim28@gmail.com>
2022-12-06 22:37:37 +0900

    신경망구조_논리회로

--- 신경망_구조_mj.py
+++ 신경망_구조_mj.py
@@ -1,21 +1,21 @@
-# AND 게이트 #가중치 만들기
+# AND 게이트 # 가중치 만들기
 import numpy as np
 import matplotlib.pyplot as plt
 
 def AND(a, b):
   input = np.array([a,b])
 
-  #가중치 설정
+  # 가중치 설정
   weights = np.array([0.4,0.4])
   bias = -0.6
 
-  #출력값
+  # 출력값
   value = np.sum(input * weights) + bias
 
 
 #---> 이렇게 하면 뉴럴 한 개의 프로그램을 짠 것!!
 
-  #반환값
+  # 반환값
   if value <= 0:
     return 0
   else:
@@ -26,6 +26,7 @@ print(AND(0,1))
 print(AND(1,0))
 print(AND(1,1))
 
+# 시각화하기
 x1 = np.arange(-2,2, 0.01)
 x2 = np.arange(-2,2, 0.01)
 bias = -0.6
@@ -57,22 +58,22 @@ plt.xlim(-0.5,1.5)
 plt.ylim(-0.5,1.5)
 plt.grid()
 
-# OR 게이트 #가중치 만들기
+# OR 게이트 # 가중치 만들기
 
 def OR(a, b):
   input = np.array([a,b])
 
-  #가중치 설정
+  # 가중치 설정
   weights = np.array([0.4,0.4])
   bias = -0.3
 
-  #출력값
+  # 출력값
   value = np.sum(input * weights) + bias
 
 
 #---> 이렇게 하면 뉴럴 한 개의 프로그램을 짠 것!!
 
-  #반환값
+  # 반환값
   if value <= 0:
     return 0
   else:
@@ -88,17 +89,17 @@ print(OR(1,1))
 def NAND(a, b):
   input = np.array([a,b])
 
-  #가중치 설정
+  # 가중치 설정
   weights = np.array([-0.6,-0.6])
   bias = 0.7
 
-  #출력값
+  # 출력값
   value = np.sum(input * weights) + bias
 
 
 #---> 이렇게 하면 뉴럴 한 개의 프로그램을 짠 것!!
 
-  #반환값
+  # 반환값
   if value <= 0:
     return 0
   else:
@@ -121,9 +122,9 @@ print(XOR(0,1))
 print(XOR(1,0))
 print(XOR(1,1))
 
-#Activation Function
+# Activation Function
 
-#Steop Function (계단 함수)
+# Step Function (계단 함수)
 def step_function(x):
   if x > 0:
     return 1
