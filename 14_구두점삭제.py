a5bba8dcbac7fc9b7b555836f5116eb1db5f028d
Rachelpoundcakes <rachelkim28@gmail.com>
2022-12-23 00:58:09 +0900

    구두점 삭제

---
+++ 14_구두점삭제.py
@@ -0,0 +1,15 @@
+# 구두점 삭제
+# 구두점을 key로 하고 값은 None인 puctuatuion 딕셔너리 만들기
+# punctuation에 있는 모든 문자를 None으로 바꾼다
+
+import unicodedata
+import sys
+text_data = ["I am Rachel Kim. I, love watching drama...",
+            "I lived in Canada in 2012. It was awesome!!",
+            "To improve your English skills, you should speak a lot."]
+punctuation = dict.fromkeys(i for i in range(sys.maxunicode)
+                            if unicodedata.category(chr(i)).startswith('p'))
+
+# 문자열의 구두점 삭제
+test = [string.translate(punctuation) for string in text_data]
+print(test)
